f2639fef4e7dadb04ac62ff135a491f2e1ed33e8
Jessie Richardson <jessie.richardson04@gmail.com>
2017-04-27 16:28:46 -0400

    Fixes page reload infinite loop bug

--- background.js
+++ background.js
@@ -2,29 +2,29 @@
 //   var checkPageButton = document.getElementById('checkPage');
 
 
+function updateIcon() {
+        chrome.browserAction.setIcon({path:"green.png"});
+        chrome.tabs.executeScript(null,{file:'content.js'});
+        alert('called');
+}
+
+// var off = true;
+//
 // function updateIcon() {
+//     if (off == true) {
+//         off = false;
 //         chrome.browserAction.setIcon({path:"green.png"});
 //         chrome.tabs.executeScript(null,{file:'content.js'});
 //         location.reload();
+//     }
+//     else {
+//         off = true;
+//         chrome.browserAction.setIcon({path:"red.png"});
+//         location.reload();
+//     }
+//     return;
 // }
-
-var off = true;
-
-function updateIcon() {
-    if (off == true) {
-        off = false;
-        chrome.browserAction.setIcon({path:"green.png"});
-        chrome.tabs.executeScript(null,{file:'content.js'});
-        location.reload();
-    }
-    else {
-        off = true;
-        chrome.browserAction.setIcon({path:"red.png"});
-        window.location.reload();
-    }
-    return;
-}
-
+//
 
 
 
